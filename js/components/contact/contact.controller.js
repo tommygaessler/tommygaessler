3a6bf66adc2e3883958a04f9b2e4871eabb3c284
Tommy Gaessler <tommy.gaessler@gmail.com>
2017-02-06 00:05:08 -0700

    added client side validation for email form

--- js/components/contact/contact.controller.js
+++ js/components/contact/contact.controller.js
@@ -10,18 +10,62 @@
 
   function contactController($scope, ContactService) {
     /*jshint validthis: true */
+
+    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
     const vm = this;
-    this.form = true;
+    vm.form = true;
+
+    vm.emailEmptyErrorMessage = false;
+    vm.emailInvalidErrorMessage = false;
+
+    vm.nameEmptyErrorMessage = false;
+
+    vm.messageEmptyErrorMessage = false;
+
 
     vm.SendGrid = function() {
-      vm.submit = true;
-      ContactService.SendGrid(vm.emailObj)
-      .then((message) => {
-        vm.message = message.data.message;
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+
+      if(angular.isUndefined(vm.emailObj)) {
+        vm.emailObj = '';
+      }
+
+      if(!emailRegex.test(vm.emailObj.from_email)) {
+        vm.emailInvalidErrorMessage = true;
+      } else {
+        vm.emailInvalidErrorMessage = false;
+      }
+
+      if(vm.emailObj.from_email === '' || vm.emailObj.from_email === undefined) {
+        vm.emailEmptyErrorMessage = true;
+      } else {
+        vm.emailEmptyErrorMessage = false;
+      }
+
+      if(vm.emailObj.name === '' || vm.emailObj.name === undefined) {
+        vm.nameEmptyErrorMessage = true;
+      } else {
+        vm.nameEmptyErrorMessage = false;
+      }
+
+      if(vm.emailObj.message === '' || vm.emailObj.message === undefined) {
+        vm.messageEmptyErrorMessage = true;
+      } else {
+        vm.messageEmptyErrorMessage = false;
+      }
+
+      if(!vm.emailInvalidErrorMessage && !vm.emailEmptyErrorMessage && !vm.nameEmptyErrorMessage && !vm.messageEmptyErrorMessage) {
+
+        vm.submit = true;
+
+        ContactService.SendGrid(vm.emailObj)
+        .then((message) => {
+          vm.message = message.data.message;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+      }
     };
   }
 
