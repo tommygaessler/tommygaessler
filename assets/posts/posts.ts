84a67f21918e23050f461aa56092b18fed5093dd
Tommy Gaessler <tommy.gaessler@gmail.com>
2017-02-22 23:02:39 -0700

    new gh-pages version

---
+++ assets/posts/posts.ts
@@ -0,0 +1,11 @@
+export var posts:Object = {
+  how_i_landed_my_dream_job_at_age_19: {
+    title: 'How I Landed my Dream Job at Age 19',
+    data: 'Feburary 2017',
+    tags: ['Techstars', 'Galvanize', 'Regis Jesuit'],
+    image: 'https://images.unsplash.com/photo-1487297977649-04b1dc408d93?dpr=2&auto=format&fit=crop&w=767&h=511&q=80&cs=tinysrgb',
+    description: 'Post-1 Description',
+    content: '<p>blah <strong class="hello">blah</strong></p>'
+  }
+}
+// social media icons
